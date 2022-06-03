33c561418ff8c0eccbbdfa07f0a577d1ddf2aa70
Wborg <wilicious@gmail.com>
2022-06-03 18:23:21 -0300

    api na azure

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from "swiper/react";
-import foto1 from '../../img/img1.png';
-import foto2 from '../../img/img2.png';
-import foto3 from '../../img/img3.png';
+import api from 'services/Api';
+import {useState,useEffect} from 'react';
+// import foto1 from '../../img/img1.png';
+// import foto2 from '../../img/img2.png';
+// import foto3 from '../../img/img3.png';
 
 
 
@@ -16,7 +18,23 @@ import "./slider.css";
 
 // import required modules
 import { Autoplay, Pagination, Navigation } from "swiper";
+
+
 function SlideShow() {
+
+  const [slide, setSlide] =  useState([]);
+
+     useEffect(() => {   
+         const getSearch = async () => {
+           try {
+             const slide = await api.get('/banner') 
+             setSlide(slide.data);
+            } catch (error){
+              console.log(error);
+            }}
+            getSearch();
+
+     }, [])
   return (
     <>
       <Swiper
@@ -34,9 +52,10 @@ function SlideShow() {
         modules={[Autoplay, Pagination, Navigation]}
         className="mySwiper"
       >
-        <SwiperSlide><img src={foto1} alt="Imagem 1" /></SwiperSlide>
+        {/* <SwiperSlide><img src={foto1} alt="Imagem 1" /></SwiperSlide>
         <SwiperSlide><img src={foto2} alt="Imagem 2" /></SwiperSlide>
-        <SwiperSlide><img src={foto3} alt="Imagem 3" /></SwiperSlide>
+        <SwiperSlide><img src={foto3} alt="Imagem 3" /></SwiperSlide> */}
+        {slide.map((slide) =>(<SwiperSlide><img  src={slide.urlBanner}/></SwiperSlide>) )}  
         
       </Swiper>
     </>
